6f9550741f1ca76372c18c8db498280ae1c58486
Lucassilv2 <lucas.032@acad.pucrs.br>
2021-10-03 16:49:32 -0300

    so falta apresentar resultado final

--- server.py
+++ server.py
@@ -11,25 +11,63 @@ bufferSize = 1024
 jogadores = []
 primeiroJogadorAcabou = 0
 # Perguntas em formato de objeto
-perguntaObj = []
+perguntaObj1 = []
+pergunta1  = question('Sabendo que uma intranet utiliza a infraestrutura de rede da empresa e fazendo uso das informações contidas no texto, considere que o computador de Paulo pode se comunicar com o computador servidor do Tribunal porque os recursos necessários estão fisicamente localizados em um raio de até 500 metros dentro do prédio do Tribunal, incluindo o computador de Paulo e o servidor. Isso significa que a rede utilizada é do tipo',[('a',"WAN."), ('b',"CAN."), ('c',"LAN.")], 'c', 0)
+perguntaObj1.append(pergunta1)
+pergunta2  = question('Para conectar sua estação de trabalho a uma rede local de computadores controlada por um servidor de domínios, o usuário dessa rede deve informar uma senha e um[a]',[('a',"endereço de FTP válido para esse domínio."), ('b',"endereço MAC de rede registrado na máquina cliente."), ('c', 'conta cadastrada e autorizada nesse domínio.')], 'c', 1)
+perguntaObj1.append(pergunta2)
+pergunta3  = question('Hoje, nas Redes Locais (LAN) cabeadas, o meio de transmissão mais utilizado é o',[('a',"Cabo de par trançado."), ('b',"Cabo de fibra óptica."), ('c',"Cabo coaxial.")], 'a', 2)
+perguntaObj1.append(pergunta3)
+pergunta4  = question('Dos equipamentos de rede abaixo, qual tem a função de escolher o melhor caminho para o envio da informação?',[('a','Switch'), ('b',"Roteador"), ('c',"Access Point")], 'a', 3)
+perguntaObj1.append(pergunta4)
+pergunta5  = question('Como é denominado o protocolo de configuração dinâmica de IP',[('a',"HTTP"), ('b',"FTP"), ('c',"DHCP")], 'c', 4)
+perguntaObj1.append(pergunta5)
+
+perguntaObj2 = []
+pergunta1  = question('Sabendo que uma intranet utiliza a infraestrutura de rede da empresa e fazendo uso das informações contidas no texto, considere que o computador de Paulo pode se comunicar com o computador servidor do Tribunal porque os recursos necessários estão fisicamente localizados em um raio de até 500 metros dentro do prédio do Tribunal, incluindo o computador de Paulo e o servidor. Isso significa que a rede utilizada é do tipo',[('a',"WAN."), ('b',"CAN."), ('c',"LAN."), ('d', 'MAN.')], 'c', 0)
+perguntaObj2.append(pergunta1)
+pergunta2  = question('Para conectar sua estação de trabalho a uma rede local de computadores controlada por um servidor de domínios, o usuário dessa rede deve informar uma senha e um[a]',[('a',"endereço de FTP válido para esse domínio."), ('b',"endereço MAC de rede registrado na máquina cliente."), ('c',"porta válida para a intranet desse domínio."), ('d', 'conta cadastrada e autorizada nesse domínio.')], 'd', 1)
+perguntaObj2.append(pergunta2)
+pergunta3  = question('Hoje, nas Redes Locais (LAN) cabeadas, o meio de transmissão mais utilizado é o',[('a',"Cabo de par trançado."), ('b',"Cabo de fibra óptica."), ('c',"Cabo coaxial."), ('d', 'Cabo Ethernet.')], 'a', 2)
+perguntaObj2.append(pergunta3)
+pergunta4  = question('Dos equipamentos de rede abaixo, qual tem a função de escolher o melhor caminho para o envio da informação?',[('a','Switch'), ('b',"Roteador"), ('c',"Access Point"), ('d', 'Patch Panel')], 'a', 3)
+perguntaObj2.append(pergunta4)
+pergunta5  = question('Como é denominado o protocolo de configuração dinâmica de IP',[('a',"HTTP"), ('b',"FTP"), ('c',"DHCP"), ('d', 'DNS')], 'c', 4)
+perguntaObj2.append(pergunta5)
+
+perguntaObj3 = []
 pergunta1  = question('Sabendo que uma intranet utiliza a infraestrutura de rede da empresa e fazendo uso das informações contidas no texto, considere que o computador de Paulo pode se comunicar com o computador servidor do Tribunal porque os recursos necessários estão fisicamente localizados em um raio de até 500 metros dentro do prédio do Tribunal, incluindo o computador de Paulo e o servidor. Isso significa que a rede utilizada é do tipo',[('a',"WAN."), ('b',"CAN."), ('c',"LAN."), ('d', 'MAN.'), ('e', 'ADSL.')], 'c', 0)
-perguntaObj.append(pergunta1)
+perguntaObj3.append(pergunta1)
 pergunta2  = question('Para conectar sua estação de trabalho a uma rede local de computadores controlada por um servidor de domínios, o usuário dessa rede deve informar uma senha e um[a]',[('a',"endereço de FTP válido para esse domínio."), ('b',"endereço MAC de rede registrado na máquina cliente."), ('c',"porta válida para a intranet desse domínio."), ('d', 'conta cadastrada e autorizada nesse domínio.'), ('e', 'certificação de navegação segura registrada na intranet.')], 'd', 1)
-perguntaObj.append(pergunta2)
+perguntaObj3.append(pergunta2)
 pergunta3  = question('Hoje, nas Redes Locais (LAN) cabeadas, o meio de transmissão mais utilizado é o',[('a',"Cabo de par trançado."), ('b',"Cabo de fibra óptica."), ('c',"Cabo coaxial."), ('d', 'Cabo Ethernet.'), ('e', 'Cabo fino 10BASE-T.')], 'a', 2)
-perguntaObj.append(pergunta3)
+perguntaObj3.append(pergunta3)
 pergunta4  = question('Dos equipamentos de rede abaixo, qual tem a função de escolher o melhor caminho para o envio da informação?',[('a','Switch'), ('b',"Roteador"), ('c',"Access Point"), ('d', 'Patch Panel'), ('e', 'Repetidor')], 'a', 3)
-perguntaObj.append(pergunta4)
+perguntaObj3.append(pergunta4)
 pergunta5  = question('Como é denominado o protocolo de configuração dinâmica de IP',[('a',"HTTP"), ('b',"FTP"), ('c',"DHCP"), ('d', 'DNS'), ('e', 'UDP')], 'c', 4)
-perguntaObj.append(pergunta5)
+perguntaObj3.append(pergunta5)
 # Perguntas em formato de json
-perguntas = json.dumps(([{"pergunta": perguntaObj[0].questionText, "respostas": perguntaObj[0].anwser, 'id': perguntaObj[0].id },
-                         {"pergunta": perguntaObj[1].questionText, "respostas": perguntaObj[1].anwser, 'id': perguntaObj[1].id },
-                         {"pergunta": perguntaObj[2].questionText, "respostas": perguntaObj[2].anwser, 'id': perguntaObj[2].id },
-                         {"pergunta": perguntaObj[3].questionText, "respostas": perguntaObj[3].anwser, 'id': perguntaObj[3].id },
-                         {"pergunta": perguntaObj[4].questionText, "respostas": perguntaObj[4].anwser, 'id': perguntaObj[4].id },
+perguntas1 = json.dumps(([
+                         {"pergunta": perguntaObj1[0].questionText, "respostas": perguntaObj1[0].anwser, 'id': perguntaObj1[0].id },
+                         {"pergunta": perguntaObj1[1].questionText, "respostas": perguntaObj1[1].anwser, 'id': perguntaObj1[1].id },
+                         {"pergunta": perguntaObj1[2].questionText, "respostas": perguntaObj1[2].anwser, 'id': perguntaObj1[2].id },
+                         {"pergunta": perguntaObj1[3].questionText, "respostas": perguntaObj1[3].anwser, 'id': perguntaObj1[3].id },
+                         {"pergunta": perguntaObj1[4].questionText, "respostas": perguntaObj1[4].anwser, 'id': perguntaObj1[4].id },
                       ]))
-perguntasLen = len(json.loads(perguntas)) 
+perguntas2 = json.dumps(([
+                         {"pergunta": perguntaObj2[0].questionText, "respostas": perguntaObj2[0].anwser, 'id': perguntaObj2[0].id },
+                         {"pergunta": perguntaObj2[1].questionText, "respostas": perguntaObj2[1].anwser, 'id': perguntaObj2[1].id },
+                         {"pergunta": perguntaObj2[2].questionText, "respostas": perguntaObj2[2].anwser, 'id': perguntaObj2[2].id },
+                         {"pergunta": perguntaObj2[3].questionText, "respostas": perguntaObj2[3].anwser, 'id': perguntaObj2[3].id },
+                         {"pergunta": perguntaObj2[4].questionText, "respostas": perguntaObj2[4].anwser, 'id': perguntaObj2[4].id },
+                      ]))
+perguntas3 = json.dumps(([
+                         {"pergunta": perguntaObj2[0].questionText, "respostas": perguntaObj3[0].anwser, 'id': perguntaObj3[0].id },
+                         {"pergunta": perguntaObj2[1].questionText, "respostas": perguntaObj3[1].anwser, 'id': perguntaObj3[1].id },
+                         {"pergunta": perguntaObj2[2].questionText, "respostas": perguntaObj3[2].anwser, 'id': perguntaObj3[2].id },
+                         {"pergunta": perguntaObj2[3].questionText, "respostas": perguntaObj3[3].anwser, 'id': perguntaObj3[3].id },
+                         {"pergunta": perguntaObj2[4].questionText, "respostas": perguntaObj3[4].anwser, 'id': perguntaObj3[4].id },
+                      ]))
 #dificuldade de jogo
 dificuldade = -1
 difficultyChange = True
@@ -37,11 +75,18 @@ difficultyChange = True
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind((localIP, localPort))
 
-def findQuestion(id):
+def findQuestion(id, perguntaObj):
     for p in perguntaObj:
         if p.id == id:
             return p
 
+def retornaQuestions(dificuldade):
+    if dificuldade == 1:
+        return perguntas1
+    elif dificuldade == 2:
+        return perguntas2
+    else:
+        return perguntas3
 
 print('Server UP')
 while(True):
@@ -50,7 +95,12 @@ while(True):
         # recebeu register do client
         if ('register' in json.loads(bytesAddressPair[0])):
             if(json.loads(bytesAddressPair[0])["register"] == True):
-                jogadores.append((bytesAddressPair[1], [0, json.loads(perguntas)]))
+                if dificuldade == 1:
+                    jogadores.append((bytesAddressPair[1], [0, json.loads(perguntas1)]))
+                if dificuldade == 2:
+                    jogadores.append((bytesAddressPair[1], [0, json.loads(perguntas2)]))
+                if dificuldade == 3:
+                    jogadores.append((bytesAddressPair[1], [0, json.loads(perguntas3)]))
                 if(dificuldade == -1):
                     msgFromPlayer = json.dumps({'setDifficulty':'Selecione a dificuldade', 'op': ['1) facil', '2) medio', '3) dificil']})
                     UDPServerSocket.sendto(bytes(msgFromPlayer, 'utf-8'), bytesAddressPair[1])
@@ -65,8 +115,9 @@ while(True):
         if ('registerDifficulty' in json.loads(bytesAddressPair[0])):
             # so pode ser alterada uma vez
             if difficultyChange:
-                dificuldade = int(json.loads(bytesAddressPair[0])["registerDifficulty"]) - 1
+                dificuldade = int(json.loads(bytesAddressPair[0])["registerDifficulty"])
                 difficultyChange = False
+            jogadores.append((bytesAddressPair[1], [0, json.loads(retornaQuestions(dificuldade))]))
             if len(jogadores) >= 2:
                 msgFromPlayer = json.dumps({'start':True})
             else:
